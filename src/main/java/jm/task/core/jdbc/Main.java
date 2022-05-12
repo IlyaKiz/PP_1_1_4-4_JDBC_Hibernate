043196adcc37573e8749638f3949cfc3930d855b
IlyaKiz <iliakiz@mail.ru>
2022-05-12 20:47:22 +0300

    12.05.2022 Home

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import java.sql.SQLException;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
+import static jm.task.core.jdbc.util.Util.statement;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+
+        getConnection();
+        statement.executeUpdate("INSERT products(ProductName, Price) VALUES ('iPhone X', 76000)," +
+                "('Galaxy S9', 45000), ('Nokia 9', 36000)");
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,34 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String USER_NAME = "debian-sys-maint";
+    public static final String PASSWORD = "GHo1kLVsMDwOvRZ4";
+    public static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static Statement statement;
+    public static Connection connection;
+    public static Connection getConnection() {
+        //Connection connection = null;
+
+
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println("connection ok");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            throw new RuntimeException();
+        }
+
+
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            throw new RuntimeException();
+        }
+
+        return connection;
+    }
 }
